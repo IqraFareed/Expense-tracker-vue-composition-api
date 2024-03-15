a88f3e921d91d50f9e7a020031620f2edfcb6b6d
iqra fareed <iqrafareed56@gmail.com>
2024-03-15 15:03:39 +0500

    compute transaction

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,67 @@
-<script>
+<script setup>
 import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
-export default {
-  components: {
-    Header,
-    Balance,
-    IncomeExpenses,
-    TransactionList,
-    AddTransaction,
-  },
+import { useToast } from "vue-toastification";
+//////////composition api/////////////
+import { ref, computed } from "vue";
+const transactions = ref([]);
+//// get total
+const total = computed(() => {
+  return transactions.value
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
+
+//// get income
+const income = computed(() => {
+  return transactions.value
+    .filter((f) => f.amount > 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
+
+//// get expenses
+const expenses = computed(() => {
+  return transactions.value
+    .filter((f) => f.amount < 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0);
+});
+const toast = useToast();
+// Add transaction
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: Math.floor(Math.random() * 100),
+    ...transactionData,
+  });
+  toast.success("Transaction added");
+};
+
+// delete transaction
+const handleTransactionDeleted = (id) => {
+  transactions.value = transactions.value.filter((obj) => obj.id !== id);
+
+  toast.success("Transaction deleted");
 };
 </script>
 
 <template>
   <Header />
   <div class="container">
-    <Balance />
-    <IncomeExpenses />
-    <TransactionList />
-    <AddTransaction />
+    <Balance :total="total" />
+    <IncomeExpenses :income="+income" :expenses="+expenses" />
+    <TransactionList
+      :transactions="transactions"
+      @transactionDeleted="handleTransactionDeleted"
+    />
+    <AddTransaction @transactionSubmitted="handleTransactionSubmitted" />
   </div>
 </template>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,13 +1,41 @@
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li class="minus">
-      Cash <span>-$400</span>
-      <button class="delete-btn">x</button>
-    </li>
-    <li class="plus">
-      Paycheck <span>$800</span>
-      <button class="delete-btn">x</button>
+    <li
+      :class="t.amount < 0 ? 'minus' : 'plus'"
+      v-for="t in transactions"
+      :key="t.id"
+    >
+      {{ t.text }} <span>${{ t.amount }}</span>
+      <button @click="deleteTransaction(t.id)" class="delete-btn">x</button>
     </li>
   </ul>
 </template>
+
+<script setup>
+import { defineProps } from "vue";
+const emit = defineEmits(["transactionDeleted"]);
+const props = defineProps({
+  transactions: {
+    type: Number,
+    required: true,
+  },
+});
+
+const deleteTransaction = (id) => {
+  emit("transactionDeleted", id);
+};
+
+////////////// options api//////////////
+// data() {
+//   return {
+//     transations: [
+//       { id: 1, text: "Flower", amount: -19.99 },
+//       { id: 2, text: "Salary", amount: 299.97 },
+//       { id: 3, text: "Book", amount: -10 },
+//       { id: 4, text: "Camera", amount: 150 },
+//     ],
+//   };
+// },
+// };
+</script>
